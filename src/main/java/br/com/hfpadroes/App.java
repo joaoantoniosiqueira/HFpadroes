e047b2b2203d2f01f41e73e25a21465a1f043286
João Antônio Ferreira Siqueira <joao@JoaoPC.ssp.ma.gov.br>
2017-01-02 09:17:46 -0300

    envio do projeto ao git

---
+++ src/main/java/br/com/hfpadroes/App.java
@@ -0,0 +1,13 @@
+package br.com.hfpadroes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
